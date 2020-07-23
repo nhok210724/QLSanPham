aa3a4bfba2505c8c6c90a0258089df5ad61fdd3b
levannghia <levannghia.tdc2018@gmail.com>
2020-07-23 21:57:49 +0700

    add new project

---
+++ QuanLySanPham/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuanLySanPham
+//
+//  Created by Le Van Nghia on 7/23/20.
+//  Copyright © 2020 Le Van Nghia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
